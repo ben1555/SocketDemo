ce528d82ab2ed11a51d0dd6a8d69504bb809b4e0
黎建斌 <501941452@qq.com>
2016-09-01 18:33:16 +0800

    Revert "Revert "SocketDemo""
    This reverts commit 0cf91d7e705cae73749bef01dc114eb434c20119.

---
+++ Assets/Editor/HEScriptKeywordReplace.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class HEScriptKeywordReplace : UnityEditor.AssetModificationProcessor
+{
+    public static void OnWillCreateAsset(string path) {
+        path = path.Replace(".meta", "");
+        int index = path.LastIndexOf(".");
+        if (index < 0) return;
+        string file = path.Substring(index);
+        if (file != ".cs" && file != ".js" && file != ".boo") return;
+        string fileExtension = file;
+
+        index = Application.dataPath.LastIndexOf("Assets");
+        path = Application.dataPath.Substring(0, index) + path;
+        file = System.IO.File.ReadAllText(path);
+
+        file = file.Replace("#CREATIONDATE#", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
+        file = file.Replace("#PROJECTNAME#", PlayerSettings.productName);
+        file = file.Replace("#SMARTDEVELOPERS#", PlayerSettings.companyName);
+        file = file.Replace("#FILEEXTENSION#", fileExtension);
+
+        System.IO.File.WriteAllText(path, file);
+        AssetDatabase.Refresh();
+    }
+}
